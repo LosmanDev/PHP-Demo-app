ae0fba6df9fe13f1bc77031dc843e639b77f1352
liban <losmandev@gmail.com>
2024-05-28 14:17:23 -0400

    first

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php ;
+
+require 'functions.php';
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if ($uri === '/') {
+    require 'controllers/index.php';
+} elseif($uri === '/about') {
+    require 'controllers/about.php';
+} elseif($uri === '/contact') {
+    require 'controllers/contact.php';
+}
+
+dd($_SERVER);
